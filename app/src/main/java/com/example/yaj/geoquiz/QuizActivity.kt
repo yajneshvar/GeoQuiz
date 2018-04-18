3dcbce704863654a2fcb1db02227db339e606ee4
yajneshvar <yajneshvar.arumugam@gmail.com>
2018-04-18 03:57:11 -0400

    Adding buttons

--- app/src/main/java/com/example/yaj/geoquiz/QuizActivity.kt
+++ app/src/main/java/com/example/yaj/geoquiz/QuizActivity.kt
@@ -2,11 +2,19 @@ package com.example.yaj.geoquiz
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class QuizActivity : AppCompatActivity() {
+
+    private lateinit var mTrueButton: Button
+    private lateinit var mFalseButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+        mTrueButton = findViewById(R.id.true_button)
+        mTrueButton.setOnClickListener({ v: View -> })
+        mFalseButton = findViewById(R.id.false_button)
     }
 }
