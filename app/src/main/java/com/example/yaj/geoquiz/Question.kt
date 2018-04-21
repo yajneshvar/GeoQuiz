85c9c3ec1a667f33398b59683813751f7234ea98
yajtraderev <yajneshvar.arumugam@traderev.com>
2018-04-21 11:26:00 -0400

    Adding questions class

---
+++ app/src/main/java/com/example/yaj/geoquiz/Question.kt
@@ -0,0 +1,3 @@
+package com.example.yaj.geoquiz
+
+class Question(var mTextResId: Int, var mAnswerTrue: Boolean)
--- app/src/main/java/com/example/yaj/geoquiz/QuizActivity.kt
+++ app/src/main/java/com/example/yaj/geoquiz/QuizActivity.kt
@@ -2,8 +2,10 @@ package com.example.yaj.geoquiz
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 
 class QuizActivity : AppCompatActivity() {
 
@@ -14,7 +16,16 @@ class QuizActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
         mTrueButton = findViewById(R.id.true_button)
-        mTrueButton.setOnClickListener({ v: View -> })
+        mTrueButton.setOnClickListener({ v: View ->
+            var toast: Toast = Toast.makeText( this, R.string.correct_toast, Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP, 0, 0)
+            toast.show()
+        })
         mFalseButton = findViewById(R.id.false_button)
+        mFalseButton.setOnClickListener({ v: View ->
+            var toast: Toast = Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP, 0, 0)
+            toast.show()
+        })
     }
 }
